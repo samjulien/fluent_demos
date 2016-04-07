cdf8140be39a196db0a2ea5d3c71f822b7437072
Sam Julien <Sam.Julien@energytrust.org>
2016-04-07 16:09:08 -0700

    remove firebase demo and fix dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core'
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, Router, RouteConfig} from 'angular2/router'
 
 import {TypeaheadDemo} from './demos/typeahead/typeahead.ts'
-import {FirebaseChat} from './demos/firebase/firebasechat.ts'
 import {TodoApp} from './demos/ngrx/todoapp.ts'
 
 @Component({
@@ -12,7 +11,6 @@ import {TodoApp} from './demos/ngrx/todoapp.ts'
       <h2>Reactive Angular2</h2>
       <div>
         <a [routerLink]="['TypeaheadDemo']">typeahead</a>
-        <a [routerLink]="['FirebaseChat']">firebase</a>
         <a [routerLink]="['TodoApp']">todos</a>
       </div>
       <router-outlet></router-outlet>
@@ -23,7 +21,6 @@ import {TodoApp} from './demos/ngrx/todoapp.ts'
 })
 @RouteConfig([
   {name: 'TypeaheadDemo', component: TypeaheadDemo, path: '/typeahead', useAsDefault: true},
-  {name: 'FirebaseChat', component: FirebaseChat, path: '/firebase'},
   {name: 'TodoApp', component: TodoApp, path: '/todos'},
 ])
 export class App {}
